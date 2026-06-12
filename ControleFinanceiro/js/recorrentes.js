60525f188183cb93c2a93da24fb1ca21440207c0
Kauê Fernandes Costa Pereira <153867670+kaue-fernandescp@users.noreply.github.com>
2026-06-12 11:55:55 -0300

    Add files via upload
    Adicionado o formulário para adicionar um novo recorrente de acordo com o seu tipo;
    Adicionado um botão de excluir para cada item.
    Políticas atualizadas no Supabase permitindo a inserção e exclusão dos itens.
    Configurada também as funções para cada função acima.

--- ControleFinanceiro/js/recorrentes.js
+++ ControleFinanceiro/js/recorrentes.js
@@ -1,7 +1,6 @@
 // Função para carregar os recorrentes
 async function carregarMenusRecorrentes() {
-    console.log("Iniciando busca no Supabase...");
-
+    //console.log("Iniciando busca no Supabase...");
     const { data: recorrentes, error } = await supabase
         .from('recorrentes')
         .select(`
@@ -41,16 +40,21 @@ function renderizarItensNoMenu(listaItens, containerId) {
         const checkedAttribute = item.rec_status ? 'checked' : '';
         const itemHTML = `
             <div class="item">
-                <div class="item-header">
-                    <input type="checkbox"
-                        class="item-checkbox"
-                        id="rec-${item.rec_id}"
-                        ${checkedAttribute}
-                        onchange="alterarStatusRecorrente(${item.rec_id}, this.checked)">
-                    <label for="rec-${item.rec_id}" class="titulo-item">${item.rec_nome}</label>
+                <div class="item-dados">
+                    <div class="item-header">
+                        <input type="checkbox"
+                            class="item-checkbox"
+                            id="rec-${item.rec_id}"
+                            ${checkedAttribute}
+                            onchange="alterarStatusRecorrente(${item.rec_id}, this.checked)">
+                        <label for="rec-${item.rec_id}" class="titulo-item">${item.rec_nome}</label>
+                    </div>
+                    <p class="valor">R$ ${Number(item.rec_valor).toFixed(2)}</p>
+                    <p class="descricao">${item.rec_descricao || 'Sem descrição.'}</p>
                 </div>
-                <p class="valor">R$ ${Number(item.rec_valor).toFixed(2)}</p>
-                <p class="descricao">${item.rec_descricao || 'Sem descrição.'}</p>
+                <div class="item-acoes">
+                    <button type="button" class="botao-excluir" onclick="excluirRecorrente(${item.rec_id})">Excluir</button>
+                </div>
             </div>
         `;
 
@@ -82,5 +86,120 @@ async function alterarStatusRecorrente(idItem, novoStatus) {
 // Executar
 document.addEventListener("DOMContentLoaded", () => {
     carregarMenusRecorrentes();
+    carregarCategoriasDropdown();
     console.log("Busca feita e retornado os itens.")
 });
+
+// Função para abrir o Form
+function abrirForm(tipoId, nomeTipo) {
+    console.log("Tentando abrir o formulário...");
+    document.getElementById('form-titulo').innerText = `Nova ${nomeTipo} Recorrente`;
+    document.getElementById('form-tipo-id').value = tipoId;
+
+    const botaoSalvar = document.querySelector('.botao-salvar');
+    if (botaoSalvar) {
+        botaoSalvar.classList.remove('estilo-entrada', 'estilo-saida');
+        
+        if (nomeTipo.toLowerCase() === 'entrada') {
+            botaoSalvar.classList.add('estilo-entrada');
+        } else {
+            botaoSalvar.classList.add('estilo-saida');
+        }
+    }
+
+    document.getElementById('form-container').style.display = 'flex';
+}
+
+// Função para fechar o Form
+function fecharForm() {
+    document.getElementById('form-container').style.display = 'none';
+    document.getElementById('form-recorrente').reset();
+
+    const btnSalvar = document.querySelector('.botao-salvar');
+    if (btnSalvar) {
+        btnSalvar.classList.remove('estilo-entrada', 'estilo-saida');
+    }
+}
+
+// Função que salvar o registro no Supabase
+async function salvarNovoRecorrente(event) {
+    event.preventDefault();
+
+    const tipoId = document.getElementById('form-tipo-id').value;
+    const nome = document.getElementById('form-nome').value;
+    const valor = document.getElementById('form-valor').value;
+    const categoriaId = document.getElementById('form-categoria').value;
+    const descricao = document.getElementById('form-descricao').value;
+
+    console.log("Enviando novo recorrente...");
+
+    const { error} = await supabase
+        .from('recorrentes')
+        .insert([
+            {
+                rec_nome: nome,
+                rec_valor: parseFloat(valor),
+                rec_descricao: descricao,
+                rec_tipo: parseInt(tipoId),
+                rec_categoria: parseInt(categoriaId),
+                rec_status: true
+            }
+        ]);
+    
+    if (error) {
+        console.error("Erro ao inserir recorrente:", error.message);
+        alert("Erro ao salvar no banco de dados.");
+        return;
+    }
+
+    console.log("Inserido com sucesso!");
+    fecharForm();
+    carregarMenusRecorrentes();
+}
+
+// Função para carregar as categorias no formulário
+async function carregarCategoriasDropdown() {
+    console.log("Buscando categorias para o dropdown...");
+
+    const { data: categorias, error } = await supabase
+        .from('categorias')
+        .select('cat_id, cat_nome');
+
+    if (error) {
+        console.error("Erro ao buscar categorias: ", error.message);
+        return;
+    }
+
+    const selectDropdown = document.getElementById('form-categoria');
+    selectDropdown.innerHTML = '<option value="" disable selected>Selecione uma categoria...</option>';
+
+    categorias.forEach(cat => {
+        const opcao = document.createElement('option');
+        opcao.value = cat.cat_id;
+        opcao.textContent = cat.cat_nome;
+        selectDropdown.appendChild(opcao);
+    });
+}
+
+// FUnção para excluir um recorrente do Supabase
+async function excluirRecorrente(idItem) {
+    const confirmar = confirm("Tem certeza que deseja excluir este item?");
+    if (!confirmar) return;
+
+    console.log(`Tentando excluir o item ID: ${idItem} no Supabase...`);
+
+    const { error } = await supabase
+        .from('recorrentes')
+        .delete()
+        .eq('rec_id', idItem);
+
+    if (error) {
+        console.error("Erro ao excluir do banco de dados: ", error.message);
+        alert("Não foi possível excluir o item.");
+        return;
+    }
+
+    console.log(`Item ${idItem} excluído com sucesso!`);
+
+    carregarMenusRecorrentes();
+}
